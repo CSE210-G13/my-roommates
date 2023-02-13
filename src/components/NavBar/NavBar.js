6c1aa18a52d000b59ec7aa6ec9804ae715a29373
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-02-13 13:14:39 -0800

    fix: correct capitalization in imports

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Link from 'next/Link';
+import Link from 'next/link';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';

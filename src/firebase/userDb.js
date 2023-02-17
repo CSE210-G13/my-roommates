f5ab7dfc126ba0d55e19fbd73c9dcef0ac9f33ab
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-02-16 16:32:23 -0800

    added post and get function from firestore

--- src/firebase/userDb.js
+++ src/firebase/userDb.js
@@ -1,8 +1,33 @@
 import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { db } from './firebaseConfig';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 export function getAuthUser() {
-    const auth = getAuth();
-    const [user, loading] = useAuthState(auth);
-    return [user, loading]
+	const auth = getAuth();
+	const [user, loading] = useAuthState(auth);
+	return [user, loading];
+}
+
+// Todo
+export async function getUser(userId) {
+	const querySnapshot = await getDocs(collection(db, 'users'));
+	querySnapshot.forEach((doc) => {
+		console.log(`${doc.id} => ${doc.data()}`);
+	});
+    return 0
+}
+
+// Todo
+export async function postUser(user) {
+	try {
+		const docRef = await addDoc(collection(db, 'users'), {
+			first: 'Ada',
+			last: 'Lovelace',
+			born: 1815,
+		});
+		console.log('Document written with ID: ', docRef);
+	} catch (e) {
+		console.error('Error adding document: ', e);
+	}
 }

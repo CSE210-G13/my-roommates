90dd79a524f87e5e8e9dd42b421d2954059961e3
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-02-21 19:11:09 -0800

    style: move everything to Paper to match with edit profile page

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,6 +2,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Unstable_Grid2";
+import Paper from "@mui/material/Paper";
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import Carousel from 'react-material-ui-carousel';
@@ -91,60 +92,61 @@ function ContactInfo({ user }) {
 
 function UserHeader({ user }) {
   return (
-    <Grid container
-      direction={{ xs: 'column', md: 'row' }}
-      columns={4}
-      spacing={4}
-      padding={4}
-      bgcolor='lightgray'
-      borderRadius={4}>
-      <Grid md={1} display="flex" alignItems="center" justifyContent="center">
-        <Avatar sx={{ fontSize: "8vh", minHeight: "15vh", minWidth: "15vh" }}>{user.firstName[0]}</Avatar>
+    <Paper variant="outlined" margin={3} padding={3}>
+      <Grid container
+        direction={{ xs: 'column', md: 'row' }}
+        columns={4}
+        spacing={4}>
+        <Grid md={1} display="flex" alignItems="center" justifyContent="center">
+          <Avatar sx={{ fontSize: "8vh", minHeight: "15vh", minWidth: "15vh" }}>{user.firstName[0]}</Avatar>
+        </Grid>
+        <Grid md={2} display="flex" alignItems="center" justifyContent="center">
+          <Stack spacing={1}>
+            <Typography variant="h3" align="center">{`${user.firstName} ${user.lastName}`}</Typography>
+            <Typography align="center">{[user.gender, user.college, user.schoolYear, `Major: ${user.major}`, `Languages: ${user.languages.join(", ")}`].join(" - ")}</Typography>
+            <Typography align="center">{user.bio}</Typography>
+          </Stack>
+        </Grid>
+        <Grid md={1} display="flex" alignItems="center" justifyContent="center">
+          <Button variant="contained">Request roommate?</Button>
+        </Grid>
+        <Grid xs={4}>
+          <ContactInfo user={user} />
+        </Grid>
       </Grid>
-      <Grid md={2} display="flex" alignItems="center" justifyContent="center">
-        <Stack spacing={1}>
-          <Typography variant="h3" align="center">{`${user.firstName} ${user.lastName}`}</Typography>
-          <Typography align="center">{[user.gender, user.college, user.schoolYear, `Major: ${user.major}`, `Languages: ${user.languages.join(", ")}`].join(" - ")}</Typography>
-          <Typography align="center">{user.bio}</Typography>
-        </Stack>
-      </Grid>
-      <Grid md={1} display="flex" alignItems="center" justifyContent="center">
-        <Button variant="contained">Request roommate?</Button>
-      </Grid>
-      <Grid xs={4}>
-        <ContactInfo user={user} />
-      </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
 function UserRoommatePref({ user }) {
   // TODO FIND NO PETS ICON
   return (
-    <Grid container columns={{ xs: 2, md: 3 }} bgcolor='lightgray' borderRadius={4} spacing={5}>
-      <Grid xs={2} md={3} display="flex" alignItems="center" justifyContent="center">
-        <Typography variant="h4" align="center">{user.firstName}'s Roommate Interests:</Typography>
+    <Paper variant="outlined" margin={3} padding={3}>
+      <Grid container columns={{ xs: 2, md: 3 }} spacing={5}>
+        <Grid xs={2} md={3} display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h4" align="center">{user.firstName}'s Roommate Interests:</Typography>
+        </Grid>
+        <RoommatePrefIcon topic="children" user={user} goodIcon={StrollerIcon} badIcon={NoStrollerIcon} />
+        <RoommatePrefIcon topic="pets" user={user} goodIcon={PetsIcon} badIcon={DoNotDisturbIcon} />
+        <RoommatePrefIcon topic="smoking" user={user} goodIcon={SmokingRoomsIcon} badIcon={SmokeFreeIcon} />
+        <RoommatePrefIcon topic="parties" user={user} goodIcon={EventAvailableIcon} badIcon={EventBusyIcon} />
+        <RoommatePrefIcon topic="alcohol" user={user} goodIcon={LocalBarIcon} badIcon={NoDrinksIcon} />
+        <RoommatePrefIcon topic="couples" user={user} goodIcon={GroupAddIcon} badIcon={GroupRemoveIcon} />
+        <Grid xs={2} sm={3} display="flex" justifyContent="center">
+          <List>
+            <TextListItem text={`Prefers ${user.roommatePref.gender.toLowerCase()} roommates`} />
+            <TextListItem text={`Prefers roommates who are also interested in ${user.roommatePref.hobbies.map((x) => x.toLowerCase()).join(", ")}`} />
+            <TextListItem text={`Prefers ${user.roommatePref.college} roommates`} />
+            <TextListItem text={`Prefers roommates majoring in ${user.roommatePref.major}`} />
+            <TextListItem text={`Prefers ${user.roommatePref.schoolYear.toLowerCase()} students`} />
+            <TextListItem text={`Prefers roommates who speak ${user.roommatePref.languages.join(", ")}`} />
+            <TextListItem text={`Prefers roommates who are ${user.roommatePref.cleanliness}/5 on cleanliness`} />
+            <TextListItem text={`Prefers roommates who are moving in around ${user.roommatePref.moveInDate}`} />
+            <TextListItem text={`Prefers roommates who sleep around ${user.roommatePref.bedtime}`} />
+          </List>
+        </Grid>
       </Grid>
-      <RoommatePrefIcon topic="children" user={user} goodIcon={StrollerIcon} badIcon={NoStrollerIcon} />
-      <RoommatePrefIcon topic="pets" user={user} goodIcon={PetsIcon} badIcon={DoNotDisturbIcon} />
-      <RoommatePrefIcon topic="smoking" user={user} goodIcon={SmokingRoomsIcon} badIcon={SmokeFreeIcon} />
-      <RoommatePrefIcon topic="parties" user={user} goodIcon={EventAvailableIcon} badIcon={EventBusyIcon} />
-      <RoommatePrefIcon topic="alcohol" user={user} goodIcon={LocalBarIcon} badIcon={NoDrinksIcon} />
-      <RoommatePrefIcon topic="couples" user={user} goodIcon={GroupAddIcon} badIcon={GroupRemoveIcon} />
-      <Grid xs={2} sm={3} display="flex" justifyContent="center">
-        <List>
-          <TextListItem text={`Prefers ${user.roommatePref.gender.toLowerCase()} roommates`} />
-          <TextListItem text={`Prefers roommates who are also interested in ${user.roommatePref.hobbies.map((x) => x.toLowerCase()).join(", ")}`} />
-          <TextListItem text={`Prefers ${user.roommatePref.college} roommates`} />
-          <TextListItem text={`Prefers roommates majoring in ${user.roommatePref.major}`} />
-          <TextListItem text={`Prefers ${user.roommatePref.schoolYear.toLowerCase()} students`} />
-          <TextListItem text={`Prefers roommates who speak ${user.roommatePref.languages.join(", ")}`} />
-          <TextListItem text={`Prefers roommates who are ${user.roommatePref.cleanliness}/5 on cleanliness`} />
-          <TextListItem text={`Prefers roommates who are moving in around ${user.roommatePref.moveInDate}`} />
-          <TextListItem text={`Prefers roommates who sleep around ${user.roommatePref.bedtime}`} />
-        </List>
-      </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
@@ -165,25 +167,27 @@ function RoommatePrefIcon({ topic, user, goodIcon, badIcon }) {
 
 function UserProperty({ user }) {
   return (
-    <Grid container columns={2} bgcolor='lightgray' borderRadius={4} spacing={5}>
-      <Grid xs={2} display="flex" alignItems="center" justifyContent="center">
-        <Typography variant="h4" align="center">{user.firstName}'s Property Interests:</Typography>
+    <Paper variant="outlined" margin={3} padding={3}>
+      <Grid container columns={2} spacing={5}>
+        <Grid xs={2} display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h4" align="center">{user.firstName}'s Property Interests:</Typography>
+        </Grid>
+        <PrefIcon icon={MapIcon} string={`Wants to be ${user.propPref.distance[0]} to ${user.propPref.distance[1]} miles away from ${user.college}`} />
+        <PrefIcon icon={AttachMoneyIcon} string={`Has a budget of $${user.propPref.budget[0]} to $${user.propPref.budget[1]} per month`} />
+        <PrefIcon icon={SingleBedIcon} string={`Wants a ${user.propPref.numBedrooms} bedroom property`} />
+        <PrefIcon icon={ShowerIcon} string={`Wants a ${user.propPref.numBathrooms} bathroom property`} />
+        <PropPrefIcon topic="pet friendly" okayWith={user.propPref.petFriendly} goodIcon={PetsIcon} badIcon={DoNotDisturbIcon} />
+        <PropPrefIcon topic="smoke free" okayWith={user.propPref.smokingBanned} goodIcon={SmokeFreeIcon} badIcon={SmokingRoomsIcon} />
+        <Grid xs={2} display="flex" justifyContent="center">
+          <List>
+            <TextListItem text={`Wants ${Object.entries(user.propPref.amenities)
+              .filter(([_, value]) => value)
+              .map(([key, _]) => { if (key === "inUnitLaundry") { return "in unit laundry" } else { return key } })
+              .join(", ")} amenities`} />
+          </List>
+        </Grid>
       </Grid>
-      <PrefIcon icon={MapIcon} string={`Wants to be ${user.propPref.distance[0]} to ${user.propPref.distance[1]} miles away from ${user.college}`} />
-      <PrefIcon icon={AttachMoneyIcon} string={`Has a budget of $${user.propPref.budget[0]} to $${user.propPref.budget[1]} per month`} />
-      <PrefIcon icon={SingleBedIcon} string={`Wants a ${user.propPref.numBedrooms} bedroom property`} />
-      <PrefIcon icon={ShowerIcon} string={`Wants a ${user.propPref.numBathrooms} bathroom property`} />
-      <PropPrefIcon topic="pet friendly" okayWith={user.propPref.petFriendly} goodIcon={PetsIcon} badIcon={DoNotDisturbIcon} />
-      <PropPrefIcon topic="smoke free" okayWith={user.propPref.smokingBanned} goodIcon={SmokeFreeIcon} badIcon={SmokingRoomsIcon} />
-      <Grid xs={2} display="flex" justifyContent="center">
-        <List>
-          <TextListItem text={`Wants ${Object.entries(user.propPref.amenities)
-            .filter(([_, value]) => value)
-            .map(([key, _]) => { if (key === "inUnitLaundry") { return "in unit laundry" } else { return key } })
-            .join(", ")} amenities`} />
-        </List>
-      </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
@@ -227,21 +231,21 @@ function PropertyCarousel({ properties, title }) {
   let groupsOf3 = groupArr(interestedProp, 3);
 
   return (
-    <Stack
-      bgcolor="lightgray"
-      borderRadius={4}
-      padding={5}
-      spacing={7}>
-      <Typography variant="h5">{title}</Typography>
-      <Carousel navButtonsAlwaysVisible={true} autoPlay={false} animation="slide">
-        {
-          groupsOf3
-            .map((items) => <Stack key={"group " + items[0] + title} direction="row" alignItems="center" justifyContent="space-evenly">
-              {items.map((x) => <Typography key={"prop " + x + title}>{x}</Typography>)}
-            </Stack>)
-        }
-      </Carousel>
-    </Stack>
+    <Paper variant="outlined" margin={3} padding={3}>
+      <Stack
+        padding={5}
+        spacing={7}>
+        <Typography variant="h5">{title}</Typography>
+        <Carousel navButtonsAlwaysVisible={true} autoPlay={false} animation="slide">
+          {
+            groupsOf3
+              .map((items) => <Stack key={"group " + items[0] + title} direction="row" alignItems="center" justifyContent="space-evenly">
+                {items.map((x) => <Typography key={"prop " + x + title}>{x}</Typography>)}
+              </Stack>)
+          }
+        </Carousel>
+      </Stack>
+    </Paper>
   )
 }
 

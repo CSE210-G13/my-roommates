7945e86038f862c9eab19bbb93c0275f43c5317e
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-02-22 23:16:40 -0800

    added maps that map display strings to User Class attributes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -66,14 +66,28 @@ export const majorsConst = [
 ];
 
 export const lifestyleConst = ['Smoke', 'Alcohol', 'Pets', 'Couples', 'Children', 'Parties']; // this has to match the lifestyle attribute in the User Classs. Not case sensitive
+export const lifestyleMap = {
+	Smoke: 'smoke',
+	Alcohol: 'alcohol',
+	Pets: 'pets',
+	Couples: 'couples',
+	Children: 'children',
+	Parties: 'parties',
+}; // map the display string to the attributes in User Class
 
 export const amenitiesConst = [
 	'Air conditioner',
 	'Swimming pool',
 	'Fitness center',
-	'Indoor washer and dryer',
-	'Covered parking',
-	'Pets allowed',
-	'Smoke-free',
-	'Access to public transportation',
+	'Indoor laundry',
+	'Parking',
+	'Public transportation',
 ];
+export const amenitiesMap = {
+	'Air conditioner': 'airConditioner',
+	'Swimming pool': 'pool',
+	'Fitness center': 'gym',
+	'Indoor laundry': 'laundry',
+	Parking: 'parking',
+	'Public transportation': 'transportation',
+}; // map the display string to the attributes in User Class

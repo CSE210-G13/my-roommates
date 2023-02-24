45bff0b8e5420b0e05d86006d6e71d1a72ecef07
bjtrieu <bjtrieu@ucsd.edu>
2023-02-23 22:28:35 -0800

    fixing errors

--- src/components/EditProfile/EditRoommatePrefForm.js
+++ src/components/EditProfile/EditRoommatePrefForm.js
@@ -11,26 +11,24 @@ import {
 	Checkbox,
 } from '@mui/material/';
 
-import { dislikesConst, hobbiesConst } from '@/constants/constants';
-
-const dislikeOptions = dislikesConst;
-const hobbiesOptions = hobbiesConst;
+import { lifestyleConst, lifestyleMap } from '@/constants/constants';
 
 export default function EditRoommatePrefForm(props) {
-	// TODO: get initial state from firebase
-    const initDislikeSelection = dislikeOptions.reduce((acc, val) => {
+	// TODO: get initial state from firebase, change to use user info and match david code
+	const initLifestyleSelection = lifestyleConst.reduce((acc, val) => {
 		acc[val] = false;
 		return acc;
 	}, {});
-    const [bedtime, setBedtime] = useState('');
-	const [dislikes, setDislike] = useState(initDislikeSelection);
 
-	const handleDislikesChange = (event) => {
-		setDislike({
-			...dislikes,
+	const handleLifestyleChange = (event) => {
+		setLifeStyle({
+			...lifestyle,
 			[event.target.name]: event.target.checked,
 		});
 	};
+
+	const [lifestyle, setLifestyle] = useState(initLifestyleSelection);
+    const [bedtime, setBedtime] = useState('');
 
     return (
         <React.Fragment>
@@ -41,7 +39,7 @@ export default function EditRoommatePrefForm(props) {
                     </Typography>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                    <FormLabel>Desired bedtime</FormLabel>
+                    <FormLabel>Usual bedtime</FormLabel>
                     <TextField
 						id="time"
 						type="time"
@@ -56,37 +54,25 @@ export default function EditRoommatePrefForm(props) {
                 </Grid>
                 <Grid item xs={12}>
 					<FormControl fullWidth disabled={!props.editing}>
-						<FormLabel>Can&apos;t accept</FormLabel>
+						<FormLabel>Lifestyle and Habits</FormLabel>
 						<FormGroup>
 							<Grid container spacing={3} rowSpacing={0.1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-								{dislikeOptions.map((dislikeString) => (
-									<Grid item xs={6} md={3} key={dislikeString}>
+								{lifestyleConst.map((lifestyleString) => (
+									<Grid item xs={6} md={3} key={lifestyleString}>
 										<FormControlLabel
 											control={
 												<Checkbox
-													name={dislikeString}
-													checked={dislikes[dislikeString]}
-													onChange={handleDislikesChange}
+													name={lifestyleString}
+													checked={lifestyle[lifestyleString]}
+													onChange={handleLifestyleChange}
 												/>
 											}
-											label={dislikeString}
+											label={lifestyleString}
 										/>
 									</Grid>
 								))}
 							</Grid>
 						</FormGroup>
-					</FormControl>
-				</Grid>
-				<Grid item xs={12}>
-					<FormControl fullWidth disabled={!props.editing}>
-						<FormLabel>Hobbies</FormLabel>
-						<Grid container spacing={3} rowSpacing={0.1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-							{hobbiesOptions.map((hobby) => (
-								<Grid item xs={6} md={3} key={hobby}>
-									<FormControlLabel control={<Checkbox />} label={hobby} />
-								</Grid>
-							))}
-						</Grid>
 					</FormControl>
 				</Grid>
             </Grid>

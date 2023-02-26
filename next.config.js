e757e120c7001691f66a71bf7416f86b5ac6f01c
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-02-25 19:39:53 -0800

    feat: set our website to link directly to roommates page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        // Redirect / (root of our website) to /roommates
+        // https://stackoverflow.com/questions/63357131/how-to-set-default-page-in-next-js#66840857h
+        source: '/',
+        destination: '/roommates',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

519af951425141ed25840d2543617cc288e9ce95
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-02-26 05:03:04 -0800

    added theme for the project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
 import '@/styles/globals.css';
-import Head from "next/head";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Head from 'next/head';
 import NavBar from '@/components/NavBar';
 import { initFirebase } from '../firebase/firebaseConfig';
+
+const theme = createTheme({
+	typography: {
+		button: {
+			textTransform: 'none',
+			fontWeight: 550,
+		},
+	},
+});
 
 export default function App({ Component, pageProps }) {
 	initFirebase();
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<Head>
 				<title>Roomie</title>
 				<meta name="description" content="Roomie by G-13" />
@@ -14,6 +24,6 @@ export default function App({ Component, pageProps }) {
 			</Head>
 			<NavBar />
 			<Component {...pageProps} />
-		</>
+		</ThemeProvider>
 	);
 }

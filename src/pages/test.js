062a0b69000946324c6a327ffb5e9e3acb64018a
adityabarsainya <aditya.barsainya16@gmail.com>
2023-03-01 08:43:01 -0800

    Changing material-ui dependency

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import {
   addUserPropertyPreference,
   getPropertyListFromUserID,

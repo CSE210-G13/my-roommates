436de6e28de737db4e47f30e0bec45d481722123
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-02-28 17:35:57 -0800

    Revert "feat: set our website to link directly to roommates page"
    This reverts commit e757e120c7001691f66a71bf7416f86b5ac6f01c.
    Reverting based on feedback that we still want to
    use the index.js file for now

--- next.config.js
+++ next.config.js
@@ -3,18 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  async redirects() {
-    return [
-      {
-        // Redirect / (root of our website) to /roommates
-        // https://nextjs.org/docs/api-reference/next.config.js/redirects
-        source: '/',
-        destination: '/roommates',
-        permanent: false,
-      },
-    ]
-  },
+  }
 }
 
 module.exports = nextConfig

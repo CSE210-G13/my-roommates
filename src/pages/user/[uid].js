e1c650723f66515e6ad906190cb05b8906b9f1da
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-03-06 13:55:24 -0800

    feat: add document title

--- src/pages/user/[uid].js
+++ src/pages/user/[uid].js
@@ -2,12 +2,19 @@ import React from 'react';
 import UserProfile from '@/components/UserProfile';
 import { getUser } from '@/firebase/userDb';
 import {getPropertyListFromUserID} from '@/firebase/userLikedProperties';
+import Head from 'next/head';
 
 export default function roommates({ user, likedProperties }) {
+	let title = `${user.firstName} ${user.lastName} | Roomies`
   return (
+		<>
+		<Head>
+			<title>{title}</title>
+		</Head>
     <main>
       <UserProfile user={user} likedProperties={likedProperties}/>
     </main>
+		</>
   )
 }
 

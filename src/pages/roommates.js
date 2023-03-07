8785e9aacf142dd3a219e9c927f473b1ccbc31b9
jn1118 <jix209@ucsd.edu>
2023-03-07 11:54:50 -0800

    display filtered roommates after clicking search button

--- src/pages/roommates.js
+++ src/pages/roommates.js
@@ -7,34 +7,17 @@ import { useState } from "react";
 
 export default function Roommates(props) {
   const [user, loading] = useAuthUser();
-
-  const [clickFilter, setClickFilter] = useState(false);
   const [FilterRoommates, setFilterRoommates] = useState([]);
-  // state = { FilterRoommates: "" };
-
-  function toggleBool() {
-    setClickFilter(!clickFilter);
-  }
-
-  // handleFilterRoommates = (FilterRoommatesIds) => {
-  //   setFilterRoommates(FilterRoommatesIds);
-  // };
-
-  //   handleLanguage = (langValue) => {
-  //     this.setState({language: langValue});
-  // }
 
   console.log("inside roommates page");
-  console.log(FilterRoommates);
   let users =
     user == null ? props.users : props.users.filter((u) => u.uid !== user.uid);
   // do not show current user in grid if they are logged in
   if (FilterRoommates.length > 0) {
     console.log(">0");
-    let users = FilterRoommates;
+    users = FilterRoommates;
   }
-  // const users =
-  //   user == null ? props.users : props.users.filter((u) => u.uid !== user.uid);
+
   return (
     <>
       <Head>
@@ -42,9 +25,7 @@ export default function Roommates(props) {
       </Head>
       <div style={{ display: "flex", padding: "10px 0px 0px 20px" }}>
         <RoommateFilter onFilteringRoommates={setFilterRoommates} />
-        {/* clickFilter ? <RoommateGrid users={users} /> :{" "} */}
         <RoommateGrid users={users} />
-        {/* <RoommateGrid users={users} /> */}
       </div>
     </>
   );

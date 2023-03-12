91fccec41f755f09a21c69b26b3fde49242ca5d5
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2023-03-12 10:35:29 -0700

    added cypress for e2e testing

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+	component: {
+		devServer: {
+			framework: 'next',
+			bundler: 'webpack',
+		},
+	},
+
+	e2e: {
+		baseUrl: 'https://localhost:3000',
+		specPattern: ['src/test/e2e/*.cy.{js,jsx,ts,tsx}'],
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+});

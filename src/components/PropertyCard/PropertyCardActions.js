b4dc5e8bf3902419508615b5bbf79660d9ac6c68
bjtrieu <bjtrieu@ucsd.edu>
2023-03-13 14:12:50 -0700

    code cleanup

--- src/components/PropertyCard/PropertyCardActions.js
+++ src/components/PropertyCard/PropertyCardActions.js
@@ -34,7 +34,6 @@ export default function PropertyCardActions({ property }) {
       getUserListFromPropertyID(property.uid)
         .then((res) => {
           let urls = [];
-          console.log(res);
           for (const uid of res) {
             getUser(uid).then((u) => {
               urls.push(u?.imageUrl);
